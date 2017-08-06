792ac40f5d6e62b4c2a729c7f13786c7a1f2e7e7
hbsnow <yuki.4uing@gmail.com>
2017-08-07 08:53:26 +0900

    add color-sheme

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var connect = require('connect')
 var serveStatic = require('serve-static')
 
 var app = connect()
-app.use(serveStatic('doc'))
+app.use(serveStatic('docs'))
 
 http.createServer(app).listen(3000)
